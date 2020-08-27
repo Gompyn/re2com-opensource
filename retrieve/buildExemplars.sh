89ab281c687372d08bb7aa305661131d7a8e6d48
Yongmin Li <liyongmin@pku.edu.cn>
2020-08-27 18:25:34 +0800

    fix buildExemplars.sh

--- retrieve/buildExemplars.sh
+++ retrieve/buildExemplars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir standard-exemplars
-./search.sh standard-corpus ../data/standard/train/train.token.code standard-exemplars/train.exemplar ../data/standard/test/test.token.code standard-exemplars/test.exemplar
+./search.sh standard-corpus ../data/standard/train/train.token.code standard-exemplars/train.exemplar ../data/standard/val/test.token.code standard-exemplars/val.exemplar ../data/standard/test/test.token.code standard-exemplars/test.exemplar
 python3 generate_exemplar.py --exemplars standard-exemplars --output ../data/standard
 
 mkdir challenge-exemplars
